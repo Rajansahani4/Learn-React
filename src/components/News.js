429d782c54a78297ddc54b259bee2ea668861018
rajan__sahani <rajansahani.1082@gmail.com>
2025-03-23 13:38:37 +0530

    basic setup of news app

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is news compo
+
+
+        <NewsItem/>
+
+      </div>
+    )
+  }
+}
+
+export default News
